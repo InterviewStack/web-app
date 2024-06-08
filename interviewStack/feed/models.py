3821f630dd05d8566bd512654f9a07cf3df50eda
Varad J <varad.gcs@gmail.com>
2024-06-09 01:07:17 +0530

    interviewStack: Slight fixups and import freshly migrated database

--- interviewStack/feed/models.py
+++ interviewStack/feed/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from core import UserProfile
+from core.models import UserProfile
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -17,9 +17,9 @@ class Comment(models.Model):
     author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     date_published = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    parent = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
 
 class Likes(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
-    comment = models.ForeignKey(Comment, on_delete=modeles.CASCADE, null=True)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
--- interviewStack/interviewStack/settings.py
+++ interviewStack/interviewStack/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     # interviewStack apps
     'core',
     'api',
+    'feed',
 ]
 
 MIDDLEWARE = [
